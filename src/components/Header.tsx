654921384b0afb08393b8e4195a06c5fc9014a67
Pauljoych <pauljoych@gmail.com>
2022-02-11 14:40:36 +0700

    Fixing img element

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,13 +8,13 @@ const Header = () => {
     <header>
       <div className="flex flex-col sm:flex-row items-center justify-between">
         <div className="flex p-4 sm:p-8">
-          <img src={logo}/>
+          <img alt="" src={logo}/>
         </div>
 
         <div className="flex flex-row space-x-6 p-4 sm:p-8 hover:cursor-pointer">
-          <a href="mailto:info@kullolabs.com"><img src={email}/></a>
-          <a href="https://github.com/KulloLabs" target="_blank"><img src={github}/></a>
-          <a href="https://github.com/KulloLabs" target="_blank"><img src={twitter}/></a>
+          <a href="mailto:info@kullolabs.com"><img alt="" src={email}/></a>
+          <a href="https://github.com/KulloLabs"><img alt="" src={github}/></a>
+          <a href="https://github.com/KulloLabs"><img alt="" src={twitter}/></a>
         </div>
       </div>
     </header>
